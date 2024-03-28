93c96247ef241fbab46bc47f96977c18300c21b9
medhagithub <144780622+medhagithub@users.noreply.github.com>
2024-03-28 18:45:55 +0530

    Add files via upload

---
+++ randomshapes.js
@@ -0,0 +1,45 @@
+const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse}= Matter;
+const engine = Engine.create()
+const {world}= engine;
+const width= 800
+const height = 600
+const render = Render.create({
+    element: document.body,
+    engine: engine,
+    Option:{
+        wireframes:true, 
+        width,
+        height
+    
+    }
+})
+Render.run(render);
+Runner.run(Runner.create(), engine)
+
+World.add(world, MouseConstraint.create(engine ,{
+    mouse: Mouse.create(render.canvas)
+}))
+
+ //const shape = Bodies.rectangle(200,200,50,50,{
+   // isStatic: true})
+    //World.add(world, shape);
+    
+    
+    //walls
+    const walls =[
+        Bodies.rectangle(400,0,800,40,{isStatic: true}),
+        Bodies.rectangle(400,600,800,40,{isStatic: true}),
+        Bodies.rectangle(0,300,40,660,{isStatic: true}),
+        Bodies.rectangle(800,300,40,600,{isStatic: true})
+
+    ]
+    World.add(world, walls)
+    for( i=0; i<20; i++){
+        if(Math.random()< 0.5){
+
+            World.add(world, Bodies.circle(Math.random()*width,Math.random()*height, 25))  
+        }
+        
+        World.add(world, Bodies.rectangle(Math.random() * width, Math.random()*height, 50,50))
+    }
+    
